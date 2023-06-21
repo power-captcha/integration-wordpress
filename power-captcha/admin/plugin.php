286663be1fc29a2e246798f90f0d2038a06c4ee8
Tim.Wittler <tim.wittler@uniiqueag.com>
2023-06-21 17:59:23 +0200

    POWI-4: Only check for updates if the user is admin, the wp cronjob is running or wp-cli is running.

--- power-captcha/admin/plugin.php
+++ power-captcha/admin/plugin.php
@@ -2,8 +2,15 @@
 
 defined('POWER_CAPTCHA_PATH') || exit;
 
-// update check
-require_once ('update-check.php');
+add_action('init', 'powercaptcha_update_check');
+function powercaptcha_update_check() {
+	if(!current_user_can('manage_options') && !wp_doing_cron() && !(defined('WP_CLI'))) {
+		return; // do nothing
+	} else {
+		// do the update check if user is admin OR cronjob is running OR wp_cli is running
+		require_once ('update-check.php');
+	}
+}
 
 // adding link to settings page on the plugin list overview
 add_filter( 'plugin_action_links_power-captcha/power-captcha.php', 'powercaptcha_settings_link' );
