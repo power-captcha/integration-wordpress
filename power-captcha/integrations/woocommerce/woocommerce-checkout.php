70cd3d6989c92a21497e7c30d675d9dfa4b627dc
Tim.Wittler <tim.wittler@uniiqueag.com>
2023-06-22 12:44:27 +0200

    POWI-127.bugfix: Fixed a bug in WooCommerce Checkout by changing the username field from "username" to "billing_email" during token verification.

--- power-captcha/integrations/woocommerce/woocommerce-checkout.php
+++ power-captcha/integrations/woocommerce/woocommerce-checkout.php
@@ -8,7 +8,9 @@ if(powercaptcha()->is_enabled(powercaptcha()::WOOCOMMERCE_CHECKOUT_INTEGRATION))
 
     // token verification
     add_action('woocommerce_after_checkout_validation', 'powercaptcha_woocommerce_checkout_verification', 10, 2);
-    // TODO: maybe use woocommerce_checkout_process or woocommerce_before_checkout_process hook?
+    // Note: We can't use the woocommerce_before_checkout_validation hook because it is executed multiple times during the checkout.
+    // Another hook alternative could be woocommerce_checkout_process, but woocommerce_after_checkout_validation seems to be the most suitable, 
+    // as it is executed after the address and payment method have been validated.
 }
 
 
@@ -138,7 +140,7 @@ function powercaptcha_woocommerce_checkout_verification(array $fields, WP_Error 
     if($pcToken === FALSE) {
         $errors->add(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD, powercaptcha_user_error_message(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD));
     } else {
-        $verification = powercaptcha_verify_token($pcToken, $_POST['username'], null, powercaptcha()::WOOCOMMERCE_CHECKOUT_INTEGRATION);
+        $verification = powercaptcha_verify_token($pcToken, $_POST['billing_email'], null, powercaptcha()::WOOCOMMERCE_CHECKOUT_INTEGRATION);
         if($verification['success'] !== TRUE) {
             $errors->add($verification['error_code'], powercaptcha_user_error_message($verification['error_code']));
         }
