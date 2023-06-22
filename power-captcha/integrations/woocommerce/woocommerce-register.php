4aea3a174ed7350ca926c34b93effd304d67286d
Tim.Wittler <tim.wittler@uniiqueag.com>
2023-06-22 12:41:06 +0200

    POWI-126.bugfix: Fixed a bug when a customer registers during WooCommerce checkout.

--- power-captcha/integrations/woocommerce/woocommerce-register.php
+++ power-captcha/integrations/woocommerce/woocommerce-register.php
@@ -7,8 +7,9 @@ if(powercaptcha()->is_enabled(powercaptcha()::WOOCOMMERCE_REGISTER_INTEGRATION))
     add_action('woocommerce_register_form_end', 'powercaptcha_woocommerce_register_integration_javascript');
 
     // token verification
-    add_action('woocommerce_register_post', 'powercaptcha_woocommerce_register_verification', 10, 3);
-    // TODO we could also use the woocommerce_registration_errors hook. but woocommerce_register_post is executed earlier.
+    add_filter('woocommerce_process_registration_errors', 'powercaptcha_woocommerce_register_verification', 10, 4);
+    // Note: We can't use the woocommerce_register_post hook because it is also executed during checkout when registering. 
+    // This would lead to problems if the captcha is also enabled for the WooCommerce checkout.
 }
 
 
@@ -77,24 +78,24 @@ function powercaptcha_woocommerce_register_integration_javascript() {
 <?php
 }
 
-function powercaptcha_woocommerce_register_verification(string $username, string $user_ermail, WP_Error $errors) {
+function powercaptcha_woocommerce_register_verification(WP_Error $validation_error, $username, $password, $email) {
     if (!powercaptcha()->is_enabled(powercaptcha()::WOOCOMMERCE_REGISTER_INTEGRATION)) {
-        return $errors;
+        return $validation_error;
     }
 
     if(empty($_POST)) {
-        return $errors;
+        return $validation_error;
     }
 
     $pcToken = powercaptcha_get_token_from_post_request();
     if($pcToken === FALSE) {
-        $errors->add(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD, powercaptcha_user_error_message(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD, false));
+        $validation_error->add(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD, powercaptcha_user_error_message(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD, false));
     } else {
         $verification = powercaptcha_verify_token($pcToken, $_POST['email'], null, powercaptcha()::WOOCOMMERCE_REGISTER_INTEGRATION);
         if($verification['success'] !== TRUE) {
-            $errors->add($verification['error_code'], powercaptcha_user_error_message($verification['error_code'], false));
+            $validation_error->add($verification['error_code'], powercaptcha_user_error_message($verification['error_code'], false));
         }
     }
 
-    return $errors;
+    return $validation_error;
 }
