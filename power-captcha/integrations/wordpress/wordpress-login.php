6d40135625dd121fe7216600e1a3da821225b77d
Tim.Wittler <tim.wittler@uniiqueag.com>
2024-03-13 19:22:15 +0100

    POWI-208.dev: Wordpress-Login integration adapted to the new Javascript (work in progress)

--- power-captcha/integrations/wordpress/wordpress-login.php
+++ power-captcha/integrations/wordpress/wordpress-login.php
@@ -3,6 +3,12 @@
 defined('POWER_CAPTCHA_PATH') || exit;
 
 if(powercaptcha()->is_enabled(powercaptcha()::WORDPRESS_LOGIN_INTEGRATION)) {
+
+    
+    // add div for the widget after password field
+    add_action('login_form', 'powercaptcha_wordpress_login_add_widget_div', 10, 2);
+
+
     // integration js
     add_action('login_form', 'powercaptcha_wordpress_login_integration_javascript');
 
@@ -20,61 +26,41 @@ function powercaptcha_wordpress_login_integration_javascript() {
 ?>
 <script type="text/javascript">
 // TODO move this script to javascript file. note parameters like apiKey and secretKey must be injected
-jQuery(function($){
+(function($){
     
     powerCaptchaWp.prefetchFrontendDetails('wordpress_login');
 
-    (function ($) {
-        const wpLoginForm = $('#loginform');
-        const wpLoginFormId = wpLoginForm.attr('id');
+    const wpLoginForm = $('#loginform');
+    const wpLoginFormId = wpLoginForm.attr('id');
         
-        // append hidden input for token
-        wpLoginForm.append('<input type="hidden" name="pc-token" value =""/>');
-        
-        // create instance for the login form
-        const captchaInstance = window.uiiCaptcha.captcha({idSuffix: wpLoginFormId, lang: powerCaptchaWp.getLang()});
+    const usernameField = wpLoginForm.find('#user_login');
 
-        // register submit listener
-        wpLoginForm.on('submit', function (event) {
-            console.debug('submitEvent for wpLoginForm', '#'+wpLoginFormId);
+    powerCaptchaWp.withFrontendDetails('wordpress_login', function(details) {
+        const captchaInstance = PowerCaptcha.init({
+            apiKey: details.apiKey,
+            backendUrl: details.backendUrl,
+            clientUid: details.clientUid,
+            widgetTarget: wpLoginForm.find('.power-captcha-widget')[0],
 
-            const tokenField = wpLoginForm.find('input[name="pc-token"]').eq(0);
-            if(tokenField.length === 0) {
-                console.warn('no pc-token field found in wpLoginForm.');
-                return; // exit
-            }
+            userInputField: wpLoginForm.find('#user_login')[0],
 
-            if(tokenField.val() === "") {
-                console.debug('pc-token field empty. preventing form submit and requesting token.');
-                event.preventDefault();
+            idSuffix: wpLoginFormId,
+            lang: details.lang,
 
-                const userNameField = wpLoginForm.find('#user_login').eq(0);
-                console.debug('userNameField val', userNameField.val());
-                const userName = userNameField.val();
-
-                powerCaptchaWp.withFrontendDetails('wordpress_login', function(details) {
-                    // requesting token
-                    captchaInstance.check({
-                        apiKey: details.apiKey,
-                        backendUrl: details.backendUrl,
-                        clientUid: details.clientUid,
-                        user: userName,
-                        callback: ''
-                    }, 
-                    function(token) {
-                        console.debug('captcha solved with token: '+token+'. setting value to tokenField.');
-                        tokenField.val(token);
-                        console.debug('resubmitting wpLoginForm.');
-                        wpLoginForm.trigger("submit");
-                    });
-                });
-            } else {
-                console.debug('pc-token already set. no token has to be requested. wpLoginForm can be submitted.');
-            }
+            invisibleMode: false, // TODO make invisibleMode configurable 
+            debug: true // TODO turn off debug or make debug configurable 
         });
-    }($));
-});
+    });
+    
+})(jQuery);
 </script>
+<?php
+}
+
+function powercaptcha_wordpress_login_add_widget_div() { 
+    // TODO control margin via css variables
+?>
+    <div class="power-captcha-widget" style="margin-bottom: 16px"></div>
 <?php
 }
 
