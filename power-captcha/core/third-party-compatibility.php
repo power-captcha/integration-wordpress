c08101d57c6fb26e50f53c2a1bacaa2351fc5913
Tim.Wittler <tim.wittler@uniiqueag.com>
2024-06-12 16:38:57 +0200

    POWI-208.bugfix: fixed WPForms integration when using WP Rocket defer option.

--- power-captcha/core/third-party-compatibility.php
+++ power-captcha/core/third-party-compatibility.php
@@ -2,11 +2,14 @@
 
 defined('POWER_CAPTCHA_PATH') || exit;
 
-// Exclude all of our JavaScript from WP Rocket delay, because the POWER CAPTCHA widget should be executed directly. 
-// This is very important for additional integration JavaScript like WPForms.
-// example: // https://github.com/wp-media/wp-rocket-helpers/blob/master/static-files/wp-rocket-static-exclude-delay-js-per-url/wp-rocket-exclude-delay-js-per-url.php
-function powercaptcha_rocket_delay_js_exclusions ( $excluded = array() ) {
+// Exclude all of our JavaScript from WP Rocket delay and defer, because the POWER CAPTCHA widget should be loaded and executed directly. 
+// Its important for additional integration JavaScript like WPForms that all scripts are loaded and executed in the right order.
+// WP Rocket Code examples:
+// Defer: https://github.com/wp-media/wp-rocket-helpers/blob/master/static-files/wp-rocket-static-exclude-defer-js/wp-rocket-static-exclude-defer-js.php
+// Delay: https://github.com/wp-media/wp-rocket-helpers/blob/master/static-files/wp-rocket-static-exclude-delay-js-per-url/wp-rocket-exclude-delay-js-per-url.php
+function powercaptcha_wp_rocket_js_exclusions ( $excluded = array() ) {
     $excluded[] = 'power-captcha(.*)\.js';
     return $excluded;
 }
-add_filter( 'rocket_delay_js_exclusions', 'powercaptcha_rocket_delay_js_exclusions');
+add_filter( 'rocket_delay_js_exclusions', 'powercaptcha_wp_rocket_js_exclusions'); // delay filter
+add_filter( 'rocket_exclude_defer_js', 'powercaptcha_wp_rocket_js_exclusions' ); // defer filter
