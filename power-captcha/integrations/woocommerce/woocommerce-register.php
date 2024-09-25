4f3bfb7ff90dec3b869435fe1658cf0b6c87ddf0
Tim.Wittler <tim.wittler@uniiqueag.com>
2024-09-25 18:16:03 +0200

    POWI-244.dev: Refactor woocommerce_register integration to object-oriented approach (WIP)

--- power-captcha/integrations/woocommerce/woocommerce-register.php
+++ power-captcha/integrations/woocommerce/woocommerce-register.php
@@ -1,45 +1,49 @@
 <?php
 
+namespace PowerCaptcha_WP;
+
 defined('POWER_CAPTCHA_PATH') || exit;
 
-if(powercaptcha()->is_enabled(powercaptcha()::WOOCOMMERCE_REGISTER_INTEGRATION)) {
-    
-    add_action('woocommerce_register_form', 'powercaptcha_woocommerce_register_widget', 100, 0);
+add_action('powercaptcha_register_integration', function ($powerCaptcha) {
+    $powerCaptcha->register_integration(new Integration_WooCommerce_Register());
+});
 
-    add_filter('woocommerce_process_registration_errors', 'powercaptcha_woocommerce_register_verification', 10, 4);
-    // Note: We can't use the woocommerce_register_post hook because it is also executed during checkout when registering. 
-    // This would lead to problems if the captcha is also enabled for the WooCommerce checkout.
-}
+class Integration_WooCommerce_Register extends Integration {
 
-
-function powercaptcha_woocommerce_register_widget() {
-    if (!powercaptcha()->is_enabled(powercaptcha()::WOOCOMMERCE_REGISTER_INTEGRATION)) {
-        return;
+    public function __construct() {
+        $this->id = 'woocommerce_register';
+        $this->setting_title = __('WooCommerce Registration', 'power-captcha');
+        $this->setting_description = __('Enable protection for the WooCommerce My Account register form.', 'power-captcha');
     }
 
-    echo powercaptcha_widget_html(powercaptcha()::WOOCOMMERCE_REGISTER_INTEGRATION, '#reg_email', true, 'form-row');
+    public function init() {
+        add_action('woocommerce_register_form', [$this, 'display_widget'], 100, 0);
+        add_action('woocommerce_register_form', [$this, 'enqueue_script'], 100, 0);
 
-    powercaptcha_enqueue_widget_script();
-}
-
-function powercaptcha_woocommerce_register_verification(WP_Error $validation_error, $username, $password, $email) {
-    if (!powercaptcha()->is_enabled(powercaptcha()::WOOCOMMERCE_REGISTER_INTEGRATION)) {
-        return $validation_error;
+        add_filter('woocommerce_process_registration_errors', [$this, 'verification'], 10, 4);
+        // Note: We can't use the woocommerce_register_post hook because it is also executed during checkout when registering. 
+        // This would lead to problems if the captcha is also enabled for the WooCommerce checkout.
     }
 
-    if(empty($_POST)) {
-        return $validation_error;
+    public function display_widget() {
+        echo $this->widget_html('#reg_email', true, 'form-row');
     }
 
-    $pcToken = powercaptcha_get_token_from_post_request();
-    if($pcToken === FALSE) {
-        $validation_error->add(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD, powercaptcha_user_error_message(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD, false));
-    } else {
-        $verification = powercaptcha_verify_token($pcToken, $_POST['email'], null, powercaptcha()::WOOCOMMERCE_REGISTER_INTEGRATION);
-        if($verification['success'] !== TRUE) {
-            $validation_error->add($verification['error_code'], powercaptcha_user_error_message($verification['error_code'], false));
+    public function enqueue_script() {
+        parent::enqueue_scripts();
+    }
+
+    public function verification(\WP_Error $validation_error,  string $username, string $password, string $email) {
+        if(empty($_POST)) {
+            return $validation_error;
         }
+    
+        $verification = $this->verify_token($_POST['email']);
+        if(FALSE === $verification->is_success()) {
+            $validation_error->add($verification->get_error_code(), $verification->get_user_message(false));
+        }
+    
+        return $validation_error; 
     }
 
-    return $validation_error;
 }
