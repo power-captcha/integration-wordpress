efc1d38716302667e4d1ac6c49ba2cf449d5adec
Tim.Wittler <tim.wittler@uniiqueag.com>
2024-09-26 16:42:42 +0200

    POWI-244.dev: Refactor wordpress_register integration to object-oriented approach (WIP)

--- power-captcha/integrations/wordpress/wordpress-register.php
+++ power-captcha/integrations/wordpress/wordpress-register.php
@@ -1,44 +1,45 @@
 <?php
 
+namespace PowerCaptcha_WP;
+
 defined('POWER_CAPTCHA_PATH') || exit;
 
-if(powercaptcha()->is_enabled(powercaptcha()::WORDPRESS_REGISTER_INTEGRATION)) {
-    
-    add_action('register_form', 'powercaptcha_wordpress_register_widget');
+add_action('powercaptcha_register_integration', function ($powerCaptcha) {
+    $powerCaptcha->register_integration(new Integration_WordPress_Register());
+});
 
-    add_action('register_post', 'powercaptcha_wordpress_register_verification', 10, 3);
-}
+class Integration_WordPress_Register extends Integration {
 
-
-function powercaptcha_wordpress_register_widget() {
-    if (!powercaptcha()->is_enabled(powercaptcha()::WORDPRESS_REGISTER_INTEGRATION)) {
-        return;
+    public function __construct() {
+        $this->id = 'wordpress_register';
+        $this->setting_title = __('WordPress Registration', 'power-captcha');
+        $this->setting_description = __('Enable protection for the WordPress registration form.', 'power-captcha');
     }
 
-    echo powercaptcha_widget_html(powercaptcha()::WORDPRESS_REGISTER_INTEGRATION, '#user_email', true, '', 'margin-bottom: 16px');
+    public function init() {
+        add_action('register_form', [$this, 'display_widget']);
+        add_action('register_form', [$this, 'enqueue_script']);
 
-    powercaptcha_enqueue_widget_script();
-}
-
-
-function powercaptcha_wordpress_register_verification(string $sanitized_user_login, string $user_email, WP_Error $errors) {
-    if (!powercaptcha()->is_enabled(powercaptcha()::WORDPRESS_REGISTER_INTEGRATION)) {
-        return;
+        add_action('register_post', [$this, 'verification'], 10, 3);
     }
 
-    if(empty($_POST)) {
-        return;
+    public function display_widget() {
+        echo $this->widget_html('#user_email', true, '', 'margin-bottom: 16px');
     }
 
-    $pcToken = powercaptcha_get_token_from_post_request();
-    if($pcToken === FALSE) {
-        $errors->add(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD, powercaptcha_user_error_message(powercaptcha()::ERROR_CODE_NO_TOKEN_FIELD));
-    } else {
-        $verification = powercaptcha_verify_token($pcToken, $user_email, null, powercaptcha()::WORDPRESS_REGISTER_INTEGRATION);
-        if($verification['success'] !== TRUE) {
-            $errors->add($verification['error_code'], powercaptcha_user_error_message($verification['error_code']));
+    public function enqueue_script() {
+        parent::enqueue_scripts();
+    }
+
+    public function verification(string $sanitized_user_login, string $user_email, \WP_Error $errors) {
+        if(empty($_POST)) {
+            return;
+        }
+
+        $verification = $this->verify_token($_POST['user_email']); 
+        if(FALSE === $verification->is_success()) {
+            $errors->add($verification->get_error_code(), $verification->get_user_message());
         }
     }
 
-    return $errors;
 }
