43898f46605dd928656e0be8ed773aa810c19947
Tim.Wittler <tim.wittler@uniiqueag.com>
2024-10-29 14:26:23 +0100

    POWI-244.dev: Skip wp_login token verification if no credentials were sent.

--- power-captcha/integrations/wordpress/wordpress-login.php
+++ power-captcha/integrations/wordpress/wordpress-login.php
@@ -34,6 +34,10 @@ class Integration_WordPress_Login extends Integration {
     public function verification(null|\WP_User|\WP_Error $user, string $username, string $password) {
         // TODO merge this verification with WooCoommerce Login integration. note: WooCommerce login uses the field $_POST['username'] for username.
 
+        if( is_wp_error($user) ) {
+            return $user;
+        }
+
         // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Missing -- Reason: The raw input is necessary and only used to verify the request via the POWER CAPTCHA API. Nonce generation and verification are handled by WordPress.
         $verification = $this->verify_token($_POST['log'] ?? null); 
         if(FALSE === $verification->is_success()) {
